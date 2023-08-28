75b16cf58e1dad159e58becbc99f8cc4e0287b1a
Charles Xavier <dev03@elzyra.com>
2023-08-28 12:01:05 -0400

    implementando provider simple

---
+++ lib/presentation/providers/hello_world_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hello_world_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$helloWorldHash() => r'd6a67360aebcc2d95c8e0b4e4acdf5ad93a62759';
+
+/// See also [helloWorld].
+@ProviderFor(helloWorld)
+final helloWorldProvider = AutoDisposeProvider<String>.internal(
+  helloWorld,
+  name: r'helloWorldProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$helloWorldHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HelloWorldRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
